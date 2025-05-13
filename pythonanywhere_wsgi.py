08c55843fc23bc94e86ed0abb03789aa407abce6
Dnyandev Sawarkar <sawarkardnyandev@gmail.com>
2025-05-14 00:51:53 +0530

    Update WSGI file with environment variables

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -2,11 +2,14 @@ import os
 import sys
 
 # Add your project directory to the sys.path
-path = '/home/yashsawarkar/Manas_Android_Research'
+path = '/home/YashSawarkar/Manas_Android_Research'
 if path not in sys.path:
-    sys.path.append(path)
+    sys.path.insert(0, path)
 
-os.environ['FLASK_ENV'] = 'production'
+# Set environment variables - IMPORTANT: These must be set before importing the app
+os.environ.setdefault('TOGETHER_API_KEY', '866abf990385a71eed854cc250f1fd4b8abd5e6b17b386f48cd795bc7572fe2a')
+os.environ.setdefault('TAVILY_API_KEY', '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')
+os.environ.setdefault('FLASK_ENV', 'production')
 
 # Import your Flask app
 from src.main import app as application
